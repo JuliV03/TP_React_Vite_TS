7b0b937718fcf0c5e0a43f1f3b3b95a515e9110b
JuliV03 <62068077+JuliV03@users.noreply.github.com>
2023-10-28 22:11:02 -0300

    Add files via upload

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+
+const Footer = () => {
+    return (
+     <footer className="mt-auto py-3 bg-dark text-white">
+      <div className="container">
+          <div className="row">
+              <div className="col-md-6 col-12">
+                  <h3>Desarrollo</h3>
+                  <p>Julieta Vicente</p>
+                  <p>Legajo 48832</p>
+                  <p>3K09</p>
+                  <p>FRM-UTN</p>
+              </div>
+              <div className="col-md-6 col-12">
+                  <h3>Otros Datos</h3>
+                  <p>Email: julietavicentemendoza@gmail.com</p>
+                  <p>Phone: 2616141960</p>
+              </div>
+          </div>
+      </div>
+     </footer>
+    )
+  }
+  
+  export default Footer
